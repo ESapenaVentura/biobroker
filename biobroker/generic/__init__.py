54c18b7f711fec419caff959331021ef8f091cbf
ESapenaVentura <sapenaventura@gmail.com>
2024-10-13 12:47:41 +0100

    Updated description

--- biobroker/generic/__init__.py
+++ biobroker/generic/__init__.py
@@ -1,3 +1,7 @@
 """
-Shared exceptions and logging. Not a lot to report here.
+Shared exceptions, logging and pydantic models. Not a lot to report here.
+
+Pydantic data models may contain more classes as validator for fields; for stand-alone models,
+use the classes marked with `Model` at the end of the class name.
 """
+
